3a108136f2f9c664a07260f4d4eb6f1059ae6dab
Alejandro Lodes <janosoft@gmail.com>
2026-06-17 21:33:39 -0300

    Settings

---
+++ CoreDBMS/Views/Configuración View/ConfiguraciónView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfiguraciónView.swift
+//  CoreDBMS
+//
+//  Created by Alejandro Lodes on 17/06/2026.
+//
+
+import SwiftUI
+
+struct Configuracio_nView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Configuracio_nView()
+}
